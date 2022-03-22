c395612aad00abd4b4362c6def1aa78457d6a891
ManYi9119 <98296363+ManYi9119@users.noreply.github.com>
2022-03-22 11:41:05 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,8 +8,8 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLScvCIrrqM9cEgiI2ZBFHrE8ejZpqTK_VGbYsUI4rdHLuCMg6w/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSe8C7QZWwo-pnUgH4KOvHwliCatAbm4bG0LREJAsBUkJ7Jfdg/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSdwStJk93hRAo69TaI2ZOC4ZcGuuwrbKet6bG0sLPCC5IbQFQ/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLSeCpV2w0-UhLCu1AvUiAQ91AgMZ3zk2a_xx0yHPhczRvWnQLQ/viewform'
+	'https://docs.google.com/     forms/d/e/1FAIpQLScvCIrrqM9cEgiI2ZBFHrE8ejZpqTK_VGbYsUI4rdHLuCMg6w/viewform',
+	'https://docs.google.com/     forms/d/e/1FAIpQLSe8C7QZWwo-pnUgH4KOvHwliCatAbm4bG0LREJAsBUkJ7Jfdg/viewform',
+	'https://docs.google.com/     forms/d/e/1FAIpQLSdwStJk93hRAo69TaI2ZOC4ZcGuuwrbKet6bG0sLPCC5IbQFQ/viewform',
+	'https://docs.google.com/     forms/d/e/1FAIpQLSeCpV2w0-UhLCu1AvUiAQ91AgMZ3zk2a_xx0yHPhczRvWnQLQ/viewform'
 ];
